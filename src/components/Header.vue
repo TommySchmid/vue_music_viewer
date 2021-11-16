61fc5f193cdbb33be2c7bcef3d51daf71bbb588b
Tommy <taschmid20@gmail.com>
2021-11-16 12:56:42 -0500

    initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="header">
+    <div>
+      <img
+        src="https://hostedimages-cdn.aweber-static.com/MTQ0MjU1NQ==/original/3250f960b0184f7498c125c0748dce50.png"
+        alt="Crate Companion"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped>
+  .header {
+    width: 100%;
+    background-color: black;
+  }
+</style>
