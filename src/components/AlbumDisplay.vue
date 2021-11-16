e88db0d3356f65e02fde76a3a41613ce5ae6d5a6
Tommy <taschmid20@gmail.com>
2021-11-16 13:47:46 -0500

    added form, albumdisplay, and trackdisplay components. Added backgroudn image

---
+++ src/components/AlbumDisplay.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>Album Display Component</div>
+</template>
+
+<script>
+export default {
+  name: "AlbumDisplay",
+};
+</script>
+
+<style></style>
---
+++ src/components/Form.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>Form Component</div>
+</template>
+
+<script>
+export default {
+  name: "Form",
+};
+</script>
+
+<style></style>
