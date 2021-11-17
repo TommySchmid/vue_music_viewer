d51c060299e8ff0ac55ac20b145910da44947516
Tommy <taschmid20@gmail.com>
2021-11-16 20:16:26 -0500

    retrieved album and track data is displayed

--- src/components/AlbumDisplay.vue
+++ src/components/AlbumDisplay.vue
@@ -1,10 +1,18 @@
 <template>
-    <div>Album Display Component</div>
+  <div>
+    Hey!
+    <div v-for="album in albums" v-bind:key="album">
+      <img :src="album.image[2]['#text']"
+        />
+      {{ album.name }}
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "AlbumDisplay",
+  props: ["albums"],
 };
 </script>
 
--- src/components/TrackDisplay.vue
+++ src/components/TrackDisplay.vue
@@ -1,10 +1,15 @@
 <template>
-    <div>Track Display Component</div>
+    <div>
+      <div v-for="track in tracks" :key="track.name">
+        {{ track.name }}
+      </div>
+    </div>
 </template>
 
 <script>
 export default {
   name: "TrackDisplay",
+  props: ["tracks"]
 };
 </script>
 
