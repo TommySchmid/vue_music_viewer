1d307074a2582de204f13a2a120cb98871f3e2cd
Tommy <taschmid20@gmail.com>
2021-11-29 22:37:46 -0500

    favorites list retrieved and displayed

---
+++ src/uml_components/UML_Fav_List.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div v-for="artist in artists" :key="artist.name">
+      {{ artist.artist }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UML_Fav_List",
+  props: ["artists"],
+};
+</script>
